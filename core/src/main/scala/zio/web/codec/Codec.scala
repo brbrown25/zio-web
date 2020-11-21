3462eee430a74d8cdb1cfe3366212d23838eb105
Jason Pickens <jasonpickensnz@gmail.com>
2020-11-22 00:48:54 +1300

    Fix unit decoder

--- core/src/main/scala/zio/web/codec/Codec.scala
+++ core/src/main/scala/zio/web/codec/Codec.scala
@@ -5,6 +5,6 @@ import zio.schema._
 import zio.stream.ZTransducer
 
 trait Codec {
-  def encoder[A](schema: Schema[A]): ZTransducer[Any, Nothing, A, Byte]
-  def decoder[A](schema: Schema[A]): ZTransducer[Any, String, Byte, A]
+  def encoder[A](schema: Schema[A]): ZTransducer[Blocking, Nothing, A, Byte]
+  def decoder[A](schema: Schema[A]): ZTransducer[Blocking, String, Byte, A]
 }
